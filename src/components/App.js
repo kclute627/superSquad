9a960e65fc1accf437273c1051ec3470a74ed787
Kyle C <kyclutter@gmail.com>
2019-01-14 20:42:30 -0600

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CharList from './CharList';
 import HeroList from './HeroList';
+import SquadStats from './SquadStats';
 import '../styles/index.css'
 
 
@@ -15,13 +16,15 @@ class App extends Component {
         return(
             <div className='App'>
                  <h2>Super Squad</h2>
-                 <div className='col-md-6'>
+                 <div className='col-md-4'>
                  <CharList />
                  </div>
-                 <div className='col-md-6'>
+                 <div className='col-md-4'>
                  <HeroList />
                  </div>
-                 
+                 <div className = 'col-md-4'>
+                     <SquadStats/>
+                 </div>
                  
             </div>
            
---
+++ src/components/SquadStats.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+
+
+
+
+
+class SquadStats extends Component {
+   
+
+    render (){
+        const strength = ()=>{
+            let strength = 0;
+            this.props.heros.forEach(cur => strength += cur.strength);
+            return strength;
+        }
+        const speed = ()=>{
+            let speed = 0;
+            this.props.heros.forEach(cur => speed += cur.speed);
+            return speed;
+        }
+        const intelligence = (a) => {
+            let intelligence = 0;
+            a.forEach(cur => intelligence += cur.intelligence);
+            return intelligence
+        }
+
+        return(
+            <div>
+                <h4>Squad Stats</h4>
+                <ul className='list-group'>
+                    <li className ="list-group-item">
+                        <b>Overall Strength:</b> {strength()}
+                    </li>
+                    <li className ="list-group-item">
+                        <b>Overall Speed:</b> {speed()}
+                    </li>
+                    <li className='list-group-item'>
+                        <b>Overall Intelligence:</b>{intelligence(this.props.heros)}
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+};
+
+const mapStateToProps = (state) => {
+
+    return {
+        heros: state.heros.heros,
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps, null)(SquadStats);
+
